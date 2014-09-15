f222fa4e7e4ce1d985675deb1a8eb5c329e03412
sekrit-twc <noreply@example.com>
2014-09-14 21:03:19 -0700

    Fix pass order heuristic

--- Resize/resize.cpp
+++ Resize/resize.cpp
@@ -110,11 +110,15 @@ void Resize::process(PixelType type, const void * RESTRICT src, void * RESTRICT 
 		double xscale = (double)m_dst_width / (double)m_src_width;
 		double yscale = (double)m_dst_height / (double)m_src_height;
 
+		// Downscaling cost is proportional to input size, whereas upscaling cost is proportional to output size.
+		// Horizontal operation is roughly twice as costly as vertical operation for SIMD cores.
+		double h_first_cost = std::max(xscale, 1.0) * 2.0 + xscale * std::max(yscale, 1.0);
+		double v_first_cost = std::max(yscale, 1.0)       + yscale * std::max(xscale, 1.0) * 2.0;
+
 		char *tmp1 = (char *)tmp;
 		char *tmp2 = tmp1 + max_frame_size(type) * pxsize;
 
-		// Minimize the number of pixels encountered by the horizontal pass.
-		if ((xscale < 1.0 && xscale * 2.0 < yscale) || (xscale >= 1.0 && xscale > yscale)) {
+		if (h_first_cost < v_first_cost) {
 			int tmp_stride = align(m_dst_width, ALIGNMENT / pxsize);
 
 			invoke_impl_h(type, src, tmp1, tmp2, m_src_width, m_src_height, src_stride, tmp_stride);
