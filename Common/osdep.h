ee14be7125b8778d7b02b599138cb7092b1a8337
sekrit-twc <noreply@example.com>
2014-10-31 00:35:57 -0700

    Add public API for project

--- Common/osdep.h
+++ Common/osdep.h
@@ -6,9 +6,11 @@
 #ifdef _MSC_VER
   #define FORCE_INLINE __forceinline
   #define RESTRICT __restrict
+  #define THREAD_LOCAL __declspec(thread)
 #else
   #define FORCE_INLINE
   #define RESTRICT
+  #define THREAD_LOCAL thread_local
 #endif
 
 #endif // ZIMG_OSDEP_H_
