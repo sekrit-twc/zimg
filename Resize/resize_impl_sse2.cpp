49e518e1312fb04c9209cd333f36e0c44dc857fd
Derek Buitenhuis <derek.buitenhuis@gmail.com>
2014-11-09 14:02:00 +0000

    resize/sse2: Remove unused variable
    Signed-off-by: Derek Buitenhuis <derek.buitenhuis@gmail.com>

--- Resize/resize_impl_sse2.cpp
+++ Resize/resize_impl_sse2.cpp
@@ -362,7 +362,6 @@ void filter_plane_fp_v(const EvaluatedFilter &filter, const ImagePlane<const flo
 	const float * RESTRICT src_p = src.data();
 	float * RESTRICT dst_p = dst.data();
 	int src_width = src.width();
-	int src_height = src.height();
 	int src_stride = src.stride();
 	int dst_stride = dst.stride();
 
