4d2831ceaa2212df119fe07a2c2ba1e1b0855a16
sekrit-twc <noreply@example.com>
2014-11-11 21:24:32 -0800

    Approximate quantization error in error diffusion, 40% speedup

--- Depth/error_diffusion.cpp
+++ Depth/error_diffusion.cpp
@@ -24,24 +24,23 @@ FloatToInteger<T> make_float_to_integer(const PixelFormat &fmt)
 }
 
 class ErrorDiffusionC : public DitherConvert {
-	template <class T, class U, class Unpack, class Quant, class Dequant>
-	void dither(const ImagePlane<const T> &src, const ImagePlane<U> &dst, float *tmp, Unpack unpack, Quant quant, Dequant dequant) const
+	template <class T, class U, class ToFloat, class FromFloat>
+	void dither(const ImagePlane<const T> &src, const ImagePlane<U> &dst, float *tmp, ToFloat to_float, FromFloat from_float) const
 	{		
 		int width = src.width();
 		int height = src.height();
 
-		float *line0 = tmp + 1;
-		float *line1 = line0 + width + 2;
-		bool even = false;
+		float quant_scale = (float)((1 << dst.format().depth) - 1);
+		float dequant_scale = 1.0f / quant_scale;
 
-		std::fill_n(tmp, ((size_t)width + 2) * 2, static_cast<T>(0));
+		float *prev_line = tmp + 1;
+		float *curr_line = prev_line + width + 2;
+
+		std::fill_n(tmp, ((size_t)width + 2) * 2, 0.0f);
 
 		for (ptrdiff_t i = 0; i < height; ++i) {
-			float *prev_line = even ? line1 : line0;
-			float *curr_line = even ? line0 : line1;
-
 			for (ptrdiff_t j = 0; j < width; ++j) {
-				float x = unpack(src[i][j]);
+				float x = to_float(src[i][j]);
 				float err = 0;
 
 				err += curr_line[j - 1] * (7.0f / 16.0f);
@@ -51,14 +50,13 @@ class ErrorDiffusionC : public DitherConvert {
 
 				x += err;
 
-				U q = quant(x);
-				float y = dequant(q);
+				float q = (float)(int)(x * quant_scale + (x < 0 ? -0.5 : 0.5)) * dequant_scale;
 
-				dst[i][j] = q;
-				curr_line[j] = x - y;
+				dst[i][j] = from_float(x);
+				curr_line[j] = x - q;
 			}
 
-			even = !even;
+			std::swap(prev_line, curr_line);
 		}
 	}
 public:
@@ -66,64 +64,56 @@ public:
 	{
 		dither(src, dst, tmp,
 		       make_integer_to_float<uint8_t>(src.format()),
-		       make_float_to_integer<uint8_t>(dst.format()),
-		       make_integer_to_float<uint8_t>(dst.format()));
+		       make_float_to_integer<uint8_t>(dst.format()));
 	}
 
 	void byte_to_word(const ImagePlane<const uint8_t> &src, const ImagePlane<uint16_t> &dst, float *tmp) const override
 	{
 		dither(src, dst, tmp,
 		       make_integer_to_float<uint8_t>(src.format()),
-		       make_float_to_integer<uint16_t>(dst.format()),
-		       make_integer_to_float<uint16_t>(dst.format()));
+		       make_float_to_integer<uint16_t>(dst.format()));
 	}
 
 	void word_to_byte(const ImagePlane<const uint16_t> &src, const ImagePlane<uint8_t> &dst, float *tmp) const override
 	{
 		dither(src, dst, tmp,
 		       make_integer_to_float<uint16_t>(src.format()),
-		       make_float_to_integer<uint8_t>(dst.format()),
-		       make_integer_to_float<uint8_t>(dst.format()));
+		       make_float_to_integer<uint8_t>(dst.format()));
 	}
 
 	void word_to_word(const ImagePlane<const uint16_t> &src, const ImagePlane<uint16_t> &dst, float *tmp) const override
 	{
 		dither(src, dst, tmp,
 		       make_integer_to_float<uint16_t>(src.format()),
-		       make_float_to_integer<uint16_t>(dst.format()),
-		       make_integer_to_float<uint16_t>(dst.format()));
+		       make_float_to_integer<uint16_t>(dst.format()));
 	}
 
 	void half_to_byte(const ImagePlane<const uint16_t> &src, const ImagePlane<uint8_t> &dst, float *tmp) const override
 	{
 		dither(src, dst, tmp,
 		       depth::half_to_float,
-		       make_float_to_integer<uint8_t>(dst.format()),
-		       make_integer_to_float<uint8_t>(dst.format()));
+		       make_float_to_integer<uint8_t>(dst.format()));
 	}
 
 	void half_to_word(const ImagePlane<const uint16_t> &src, const ImagePlane<uint16_t> &dst, float *tmp) const override
 	{
 		dither(src, dst, tmp,
 		       depth::half_to_float,
-		       make_float_to_integer<uint16_t>(dst.format()),
-		       make_integer_to_float<uint16_t>(dst.format()));
+		       make_float_to_integer<uint16_t>(dst.format()));
 	}
 
 	void float_to_byte(const ImagePlane<const float> &src, const ImagePlane<uint8_t> &dst, float *tmp) const override
 	{
 		dither(src, dst, tmp,
 		       identity<float>,
-		       make_float_to_integer<uint8_t>(dst.format()),
-		       make_integer_to_float<uint8_t>(dst.format()));
+		       make_float_to_integer<uint8_t>(dst.format()));
 	}
 
 	void float_to_word(const ImagePlane<const float> &src, const ImagePlane<uint16_t> &dst, float *tmp) const override
 	{
 		dither(src, dst, tmp,
 		       identity<float>,
-		       make_float_to_integer<uint16_t>(dst.format()),
-		       make_integer_to_float<uint16_t>(dst.format()));
+		       make_float_to_integer<uint16_t>(dst.format()));
 	}
 };
 
