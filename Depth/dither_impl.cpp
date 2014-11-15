88e29867c59b00e320f240f52243ada0a51ff7a4
sekrit-twc <noreply@example.com>
2014-11-14 20:20:23 -0800

    Change OrderedDither to use a fixed number of offsets

--- Depth/dither_impl.cpp
+++ Depth/dither_impl.cpp
@@ -15,10 +15,6 @@ namespace depth {;
 
 namespace {;
 
-static const int RANDOM_DITHER_NUMBER = 128 * 128;
-static const int RANDOM_DITHER_PERIOD = 128;
-
-// Divide by 65.
 static const unsigned short ORDERED_DITHERS[64] = {
 	 1, 49, 13, 61,  4, 52, 16, 64,
 	33, 17, 45, 29, 36, 20, 48, 32,
@@ -30,37 +26,36 @@ static const unsigned short ORDERED_DITHERS[64] = {
 	43, 27, 39, 23, 42, 26, 38, 22	
 };
 
+static const int ORDERED_DITHERS_SCALE = 65;
+
 template <class T>
 float normalize_dither(T x, T min, T max)
 {
 	return static_cast<float>(x - min) / static_cast<float>(max) - 0.5f;
 }
 
-std::pair<int, int> get_none_dithers(float *p)
+void get_none_dithers(float *p)
 {
-	std::fill_n(p, 8, 0.0f);
-	return{ 8, 8 };
+	std::fill_n(p, OrderedDither::NUM_DITHERS, 0.0f);
 }
 
-std::pair<int, int> get_ordered_dithers(float *p)
+void get_ordered_dithers(float *p)
 {
-	for (unsigned i : ORDERED_DITHERS) {
-		*p++ = normalize_dither(i, 0U, 65U);
+	for (int i = 0; i < OrderedDither::NUM_DITHERS_H; ++i) {
+		for (int j = 0; j < OrderedDither::NUM_DITHERS_V; ++j) {
+			*p++ = normalize_dither((int)ORDERED_DITHERS[(i % 8) * 8 + j % 8], 0, ORDERED_DITHERS_SCALE);
+		}
 	}
-
-	return{ 64, 8 };
 }
 
-std::pair<int, int> get_random_dithers(float *p)
+void get_random_dithers(float *p)
 {
 	std::mt19937 mt;
 	uint_fast32_t mt_min = std::mt19937::min();
 	uint_fast32_t mt_max = std::mt19937::max();
 
 	// Dividing the random numbers by 4 chosen arbitrarily to limit noisiness.
-	std::generate_n(p, RANDOM_DITHER_NUMBER, [&]() { return normalize_dither(mt(), mt_min, mt_max) * 0.25; });
-
-	return{ RANDOM_DITHER_NUMBER, RANDOM_DITHER_PERIOD };
+	std::generate_n(p, OrderedDither::NUM_DITHERS, [&]() { return normalize_dither(mt(), mt_min, mt_max) * 0.25; });
 }
 
 template <class T>
@@ -80,23 +75,20 @@ class OrderedDitherC : public OrderedDither {
 	void dither(const ImagePlane<const T> &src, const ImagePlane<U> &dst, float *tmp, int depth, ToFloat to_float, FromFloat from_float) const
 	{
 		int width = src.width();
-		int height = dst.height();
+		int height = src.height();
 
-		int hperiod = m_period;
-		int vperiod = (int)(m_dither.size() / hperiod);
 		float scale = 1.0f / (float)(1 << (depth - 1));
-
 		auto dither_pixel = [=](T x, float d) { return from_float(to_float(x) + d * scale); };
 
 		for (ptrdiff_t i = 0; i < height; ++i) {
-			int loop_end = mod(width, hperiod);
+			ptrdiff_t loop_end = mod(width, NUM_DITHERS_H);
 			int m;
 
-			const float *dith = m_dither.data() + (i % vperiod) * hperiod;
+			const float *dith = m_dither.data() + (i % NUM_DITHERS_V) * NUM_DITHERS_H;
 
-			for (ptrdiff_t j = 0; j < loop_end; j += hperiod) {
+			for (ptrdiff_t j = 0; j < loop_end; j += NUM_DITHERS_H) {
 				m = 0;
-				for (ptrdiff_t jj = j; jj < j + hperiod; ++jj) {
+				for (ptrdiff_t jj = j; jj < j + NUM_DITHERS_H; ++jj) {
 					dst[i][jj] = dither_pixel(src[i][jj], dith[m++]);
 				}
 			}
@@ -108,7 +100,7 @@ class OrderedDitherC : public OrderedDither {
 		}
 	}
 public:
-	OrderedDitherC(const float *first, const float *last, int period) : OrderedDither(first, last, period)
+	OrderedDitherC(const float *dither) : OrderedDither(dither)
 	{}
 
 	void byte_to_byte(const ImagePlane<const uint8_t> &src, const ImagePlane<uint8_t> &dst, float *tmp) const override
@@ -159,32 +151,31 @@ public:
 } // namespace
 
 
-OrderedDither::OrderedDither(const float *first, const float *last, int period) :
-	m_dither(first, last),
-	m_period{ period }
+OrderedDither::OrderedDither(const float *dither) :
+	m_dither(dither, dither + NUM_DITHERS)
 {
 }
 
 DitherConvert *create_ordered_dither(DitherType type, CPUClass cpu)
 {
-	std::vector<float> dithers(RANDOM_DITHER_NUMBER);
-	std::pair<int, int> dither_size{};
+	DitherConvert *ret = nullptr;
+	float dither[OrderedDither::NUM_DITHERS];
 
 	switch (type) {
 	case DitherType::DITHER_NONE:
-		dither_size = get_none_dithers(dithers.data());
+		get_none_dithers(dither);
 		break;
 	case DitherType::DITHER_ORDERED:
-		dither_size = get_ordered_dithers(dithers.data());
+		get_ordered_dithers(dither);
 		break;
 	case DitherType::DITHER_RANDOM:
-		dither_size = get_random_dithers(dithers.data());
+		get_random_dithers(dither);
 		break;
 	default:
 		throw ZimgIllegalArgument{ "unrecognized ordered dither type" };
 	}
 
-	return new OrderedDitherC{ dithers.data(), dithers.data() + dither_size.first, dither_size.second };
+	return new OrderedDitherC{ dither };
 }
 
 } // namespace depth
