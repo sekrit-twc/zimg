0d382d342025ae8cd605b82998b2ff4c35983907
sekrit-twc <noreply@example.com>
2015-09-19 12:22:21 -0700

    unit: add sha1 implementation
    Stolen from redis: http://download.redis.io/redis-stable/src/

---
+++ UnitTest/Extra/sha1/config.h
@@ -0,0 +1,11 @@
+#ifndef LITTLE_ENDIAN
+  #define LITTLE_ENDIAN 4321
+#endif
+
+#ifndef BIG_ENDIAN
+  #define BIG_ENDIAN 1234
+#endif
+
+#ifndef BYTE_ORDER
+  #define BYTE_ORDER LITTLE_ENDIAN
+#endif
