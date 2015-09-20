7ae752c369244e8d5ad1ea75e9a2cd5c54bca76a
sekrit-twc <noreply@example.com>
2015-09-20 14:09:30 -0700

    unit: patch math library functions with musl

--- UnitTest/main.cpp
+++ UnitTest/main.cpp
@@ -1,10 +1,23 @@
 #include <cstdio>
 #include <cstdlib>
+#include "Common/libm_wrapper.h"
+
 #include "gtest/gtest.h"
+#include "musl-libm/mymath.h"
+
+void wrap_libm()
+{
+	_zimg_sin = mysin;
+	_zimg_cos = mycos;
+	_zimg_pow = mypow;
+	_zimg_powf = mypowf;
+}
 
 int main(int argc, char **argv)
 {
 	int ret;
+
+	wrap_libm();
 
 	::testing::InitGoogleTest(&argc, argv);
 	ret = RUN_ALL_TESTS();
