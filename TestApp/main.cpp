4a1de8e35dccb0ebb9377bbe8fdb2c01c702821f
sekrit-twc <noreply@example.com>
2015-09-22 23:29:59 -0700

    testapp: cleanup main.cpp

--- TestApp/main.cpp
+++ TestApp/main.cpp
@@ -1,9 +1,12 @@
-#include <cstring>
 #include <iostream>
 #include "Common/except.h"
+#include "Common/static_map.h"
+
 #include "apps.h"
 
 namespace {;
+
+typedef int (*main_func)(int, char **);
 
 void usage()
 {
@@ -14,35 +17,40 @@ void usage()
 	//std::cout << "    unresize   - unresize images\n";
 }
 
+main_func lookup_app(const char *name)
+{
+	static const zimg::static_string_map<main_func, 1> map{
+		{ "", nullptr }
+	};
+
+	auto it = map.find(name);
+	return it == map.end() ? nullptr : it->second;
+}
+
 } // namespace
 
 
-int main(int argc, const char **argv)
+int main(int argc, char **argv)
 {
 	if (argc < 2) {
 		usage();
-		return -1;
+		return 1;
 	}
 
 	try {
-#if 0
-		if (!strcmp(argv[1], "colorspace")) {
-			return colorspace_main(argc - 1, argv + 1);
-		} else if (!strcmp(argv[1], "depth")) {
-			return depth_main(argc - 1, argv + 1);
-		} else if (!strcmp(argv[1], "resize")) {
-			return resize_main(argc - 1, argv + 1);
-		} else if (!strcmp(argv[1], "unresize")) {
-			return unresize_main(argc - 1, argv + 1);
-		} else {
+		main_func func = lookup_app(argv[1]);
+
+		if (!func) {
 			usage();
-			return -1;
+			return 1;
 		}
-#endif
-		return 0;
-	} catch (const zimg::ZimgException &e) {
+
+		return func(argc - 1, argv + 1);
+	} catch (const zimg::error::Exception &e) {
 		std::cerr << e.what() << '\n';
+		return 2;
 	} catch (const std::exception &e) {
 		std::cerr << e.what() << '\n';
+		return 2;
 	}
 }
