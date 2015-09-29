fe76787bc2b4d7ca99a20071916b86943aabb696
sekrit-twc <noreply@example.com>
2015-09-28 20:06:46 -0700

    depth: add SSE2 ordered dither
    On Haswell, dithering 2480x3507 image:
    
    Conversion	cycles/pixel
    byte->byte	        1.32
    byte->word	        1.40
    word->byte	        1.29
    word->word	        1.37
    float->byte	        1.24
    float->word	        1.31

--- Depth/dither2_x86.cpp
+++ Depth/dither2_x86.cpp
@@ -1,13 +1,52 @@
 #ifdef ZIMG_X86
 
+#include "Common/cpuinfo.h"
+#include "Common/pixel.h"
 #include "dither2_x86.h"
 
 namespace zimg {;
 namespace depth {;
 
+namespace {;
+
+dither_convert_func select_ordered_dither_func_sse2(PixelType pixel_in, PixelType pixel_out)
+{
+	if (pixel_in == PixelType::HALF)
+		pixel_in = PixelType::FLOAT;
+
+	if (pixel_in == PixelType::BYTE && pixel_out == PixelType::BYTE)
+		return ordered_dither_b2b_sse2;
+	else if (pixel_in == PixelType::BYTE && pixel_out == PixelType::WORD)
+		return ordered_dither_b2w_sse2;
+	else if (pixel_in == PixelType::WORD && pixel_out == PixelType::BYTE)
+		return ordered_dither_w2b_sse2;
+	else if (pixel_in == PixelType::WORD && pixel_out == PixelType::WORD)
+		return ordered_dither_w2w_sse2;
+	else if (pixel_in == PixelType::FLOAT && pixel_out == PixelType::BYTE)
+		return ordered_dither_f2b_sse2;
+	else if (pixel_in == PixelType::FLOAT && pixel_out == PixelType::WORD)
+		return ordered_dither_f2w_sse2;
+	else
+		return nullptr;
+}
+
+} // namespace
+
+
 dither_convert_func select_ordered_dither_func_x86(const PixelFormat &pixel_in, const PixelFormat &pixel_out, CPUClass cpu)
 {
-	return nullptr;
+	X86Capabilities caps = query_x86_capabilities();
+	dither_convert_func func = nullptr;
+
+	if (cpu == CPUClass::CPU_AUTO) {
+		if (!func && caps.sse2)
+			func = select_ordered_dither_func_sse2(pixel_in.type, pixel_out.type);
+	} else {
+		if (!func && cpu >= CPUClass::CPU_X86_SSE2)
+			func = select_ordered_dither_func_sse2(pixel_in.type, pixel_out.type);
+	}
+
+	return func;
 }
 
 dither_f16c_func select_dither_f16c_func_x86(CPUClass cpu)
--- Depth/dither2_x86.h
+++ Depth/dither2_x86.h
@@ -10,6 +10,24 @@
 namespace zimg {;
 namespace depth {;
 
+void ordered_dither_b2b_sse2(const float *dither, unsigned dither_offset, unsigned dither_mask,
+                             const void *src, void *dst, float scale, float offset, unsigned bits, unsigned left, unsigned right);
+
+void ordered_dither_b2w_sse2(const float *dither, unsigned dither_offset, unsigned dither_mask,
+                             const void *src, void *dst, float scale, float offset, unsigned bits, unsigned left, unsigned right);
+
+void ordered_dither_w2b_sse2(const float *dither, unsigned dither_offset, unsigned dither_mask,
+                             const void *src, void *dst, float scale, float offset, unsigned bits, unsigned left, unsigned right);
+
+void ordered_dither_w2w_sse2(const float *dither, unsigned dither_offset, unsigned dither_mask,
+                             const void *src, void *dst, float scale, float offset, unsigned bits, unsigned left, unsigned right);
+
+void ordered_dither_f2b_sse2(const float *dither, unsigned dither_offset, unsigned dither_mask,
+                             const void *src, void *dst, float scale, float offset, unsigned bits, unsigned left, unsigned right);
+
+void ordered_dither_f2w_sse2(const float *dither, unsigned dither_offset, unsigned dither_mask,
+                             const void *src, void *dst, float scale, float offset, unsigned bits, unsigned left, unsigned right);
+
 dither_convert_func select_ordered_dither_func_x86(const PixelFormat &pixel_in, const PixelFormat &pixel_out, CPUClass cpu);
 
 dither_f16c_func select_dither_f16c_func_x86(CPUClass cpu);
