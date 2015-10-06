dfba2166f9ea6236e52d6ca1f765ed1e64740951
sekrit-twc <noreply@example.com>
2015-10-05 22:30:44 -0700

    common: backport std::make_unique from C++14

---
+++ src/zimg/common/make_unique.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#ifndef ZIMG_MAKE_UNIQUE_H_
+#define ZIMG_MAKE_UNIQUE_H_
+
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+namespace ztd {;
+
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique_helper(std::false_type, Args&&... args) {
+	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique_helper(std::true_type, Args&&... args) {
+	static_assert(std::extent<T>::value == 0,
+	              "make_unique<T[N]>() is forbidden, please use make_unique<T[]>().");
+
+	typedef typename std::remove_extent<T>::type U;
+	return std::unique_ptr<T>(new U[sizeof...(Args)]{ std::forward<Args>(args)... });
+}
+
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+	return make_unique_helper<T>(std::is_array<T>(), std::forward<Args>(args)...);
+}
+
+} // namespace ztd
+
+#endif // ZIMG_MAKE_UNIQUE_H_
