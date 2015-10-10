2cfab8b797d58c751778c22f15c30918af74dea5
sekrit-twc <noreply@example.com>
2015-10-10 06:55:21 -0700

    testcommon: add JSON parser

---
+++ src/testcommon/json.cpp
@@ -0,0 +1,450 @@
+#include <algorithm>
+#include <cctype>
+#include <stdexcept>
+#include <tuple>
+#include <utility>
+#include <vector>
+#include "json.h"
+
+namespace {;
+
+struct Token;
+
+typedef std::string::const_iterator string_iterator;
+typedef std::vector<Token>::const_iterator token_iterator;
+
+struct Token {
+	enum tag_type {
+		NULL_,
+		CURLY_BRACE_LEFT,
+		CURLY_BRACE_RIGHT,
+		SQUARE_BRACE_LEFT,
+		SQUARE_BRACE_RIGHT,
+		COMMA,
+		COLON,
+		STRING_LITERAL,
+		NUMBER_LITERAL,
+		TRUE_,
+		FALSE_
+	} tag;
+
+	std::string str;
+
+	Token(nullptr_t = nullptr) : tag{ NULL_ }
+	{
+	}
+
+	Token(tag_type t, std::string s = {}) : tag{ t }, str{ s }
+	{
+	}
+
+	explicit operator bool() const
+	{
+		return tag != NULL_;
+	}
+};
+
+template <class InputIt1, class InputIt2>
+bool equal_prefix(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2)
+{
+	if (std::distance(first2, last2) < std::distance(first1, last1))
+		return false;
+	else
+		return std::equal(first1, last1, first2);
+}
+
+template <class Pred>
+string_iterator skip_if(string_iterator pos, string_iterator last, Pred pred)
+{
+	while (pos != last && pred(*pos)) {
+		++pos;
+	}
+	return pos;
+}
+
+std::pair<Token, string_iterator> match_token_string_literal(string_iterator pos, string_iterator last)
+{
+	string_iterator first = pos;
+	char prev_c;
+
+	if ((prev_c = *pos++) != '"')
+		throw std::invalid_argument{ "bad character" };
+
+	while (pos != last && (*pos != '"' && prev_c != '\\')) {
+		prev_c = *pos++;
+	}
+
+	if (pos == last)
+		throw std::runtime_error{ "expected '\"' before end of string" };
+
+	++pos;
+	return{ { Token::STRING_LITERAL, { first, pos } }, pos };
+}
+
+std::pair<Token, string_iterator> match_token_number_literal(string_iterator pos, string_iterator last)
+{
+	string_iterator first = pos;
+
+	if (*pos == '-')
+		++pos;
+
+	if (pos == last)
+		throw std::runtime_error{ "expected a digit" };
+
+	pos = skip_if(pos, last, isdigit);
+
+	if (pos != last && (*pos == '.')) {
+		++pos;
+		pos = skip_if(pos, last, isdigit);
+	}
+
+	if (pos != last && (*pos == 'e' || *pos == 'E')) {
+		++pos;
+
+		if (pos != last && (*pos == '-' || *pos == '+'))
+			++pos;
+		if (pos == last)
+			throw std::runtime_error{ "expected an exponent" };
+
+		pos = skip_if(pos, last, isdigit);
+	}
+
+	return{ { Token::NUMBER_LITERAL, { first, pos } }, pos };
+}
+
+std::pair<Token, string_iterator> match_token_keyword(string_iterator pos, string_iterator last)
+{
+	std::string true_str{ "true" };
+	std::string false_str{ "false" };
+	std::string null_str{ "null" };
+
+	if (equal_prefix(true_str.begin(), true_str.end(), pos, last))
+		return{ Token::TRUE_, pos + true_str.size() };
+	else if (equal_prefix(false_str.begin(), false_str.end(), pos, last))
+		return{ Token::FALSE_, pos + false_str.size() };
+	else if (equal_prefix(null_str.begin(), null_str.end(), pos, last))
+		return{ Token::NULL_, pos + null_str.size() };
+	else
+		throw std::runtime_error{ "expected a keyword" };
+}
+
+std::pair<Token, string_iterator> match_token(string_iterator pos, string_iterator last)
+{
+	if (isspace(*pos))
+		return{ nullptr, skip_if(pos, last, isspace) };
+	else if (*pos == '{')
+		return{ Token::CURLY_BRACE_LEFT, pos + 1 };
+	else if (*pos == '}')
+		return{ Token::CURLY_BRACE_RIGHT, pos + 1 };
+	else if (*pos == '[')
+		return{ Token::SQUARE_BRACE_LEFT, pos + 1 };
+	else if (*pos == ']')
+		return{ Token::SQUARE_BRACE_RIGHT, pos + 1 };
+	else if (*pos == ',')
+		return{ Token::COMMA, pos + 1 };
+	else if (*pos == ':')
+		return{ Token::COLON, pos + 1 };
+	else if (*pos == '"')
+		return match_token_string_literal(pos, last);
+	else if (*pos == '-' || isdigit(*pos))
+		return match_token_number_literal(pos, last);
+	else
+		return match_token_keyword(pos, last);
+}
+
+std::vector<Token> tokenize(const std::string &str)
+{
+	std::vector<Token> tokens;
+
+	string_iterator pos = str.begin();
+	string_iterator last = str.end();
+
+	while (pos != last) {
+		auto tok = match_token(pos, last);
+
+		if (tok.first)
+			tokens.emplace_back(std::move(tok.first));
+
+		pos = tok.second;
+	}
+
+	return tokens;
+}
+
+
+std::string decode_string_literal(const std::string &str)
+{
+	std::string ret;
+	bool is_escape = false;
+
+	if (str.size() < 2)
+		throw std::invalid_argument{ "bad string literal" };
+
+	for (string_iterator it = str.begin() + 1; it != str.end() - 1; ++it) {
+		if (is_escape) {
+			char x;
+
+			switch (*it) {
+			case 'b':
+				x = '\b';
+				break;
+			case 'f':
+				x = '\f';
+				break;
+			case 'n':
+				x = '\n';
+				break;
+			case 'r':
+				x = '\r';
+				break;
+			case 't':
+				x = '\t';
+				break;
+			case 'u':
+				throw std::runtime_error{ "unicode not supported" };
+			default:
+				x = *it;
+				break;
+			}
+
+			ret.push_back(x);
+			is_escape = false;
+		} else if (*it == '\\') {
+			is_escape = true;
+		} else {
+			ret.push_back(*it);
+		}
+	}
+	return ret;
+}
+
+double decode_number_literal(const std::string &str)
+{
+	try {
+		size_t pos;
+		double d = std::stod(str, &pos);
+
+		if (pos != str.size())
+			throw std::runtime_error{ "bad number literal" };
+
+		return d;
+	} catch (std::logic_error &e) {
+		throw std::runtime_error{ e.what() };
+	}
+}
+
+token_iterator expect_token(Token::tag_type type, token_iterator pos, token_iterator last)
+{
+	if (pos == last)
+		throw std::runtime_error{ "expected a token" };
+	if (pos->tag != type)
+		throw std::runtime_error{ "unexpected token type" };
+	return ++pos;
+}
+
+std::pair<JsonValue, token_iterator> parse_value(token_iterator pos, token_iterator last);
+
+std::pair<JsonValue, token_iterator> parse_object(token_iterator pos, token_iterator last);
+
+std::pair<JsonValue, token_iterator> parse_array(token_iterator pos, token_iterator last)
+{
+	std::vector<JsonValue> array;
+
+	pos = expect_token(Token::SQUARE_BRACE_LEFT, pos, last);
+
+	while (pos != last && pos->tag != Token::SQUARE_BRACE_RIGHT) {
+		auto val = parse_value(pos, last);
+
+		array.emplace_back(std::move(val.first));
+		pos = val.second;
+
+		if (pos == last)
+			throw std::runtime_error{ "expected ']' before end of string" };
+		if (pos->tag == Token::SQUARE_BRACE_RIGHT)
+			break;
+
+		pos = expect_token(Token::COMMA, pos, last);
+	};
+	pos = expect_token(Token::SQUARE_BRACE_RIGHT, pos, last);
+
+	return{ array, pos };
+}
+
+std::pair<JsonValue, token_iterator> parse_object(token_iterator pos, token_iterator last)
+{
+	JsonObject obj;
+
+	pos = expect_token(Token::CURLY_BRACE_LEFT, pos, last);
+
+	while (pos != last && pos->tag != Token::CURLY_BRACE_RIGHT) {
+		expect_token(Token::STRING_LITERAL, pos, last);
+
+		std::string name = decode_string_literal((pos++)->str);
+		pos = expect_token(Token::COLON, pos, last);
+
+		std::tie(obj[name], pos) = parse_value(pos, last);
+
+		if (pos == last)
+			throw std::runtime_error{ "expected '}' before end of string" };
+		if (pos->tag == Token::CURLY_BRACE_RIGHT)
+			break;
+
+		pos = expect_token(Token::COMMA, pos, last);
+	};
+	pos = expect_token(Token::CURLY_BRACE_RIGHT, pos, last);
+
+	return{ std::move(obj), pos };
+}
+
+std::pair<JsonValue, token_iterator> parse_value(token_iterator pos, token_iterator last)
+{
+	if (pos == last)
+		throw std::runtime_error{ "expected a token" };
+
+	switch (pos->tag) {
+	case Token::NUMBER_LITERAL:
+		return{ decode_number_literal(pos->str), pos + 1 };
+	case Token::STRING_LITERAL:
+		return{ decode_string_literal(pos->str), pos + 1 };
+	case Token::SQUARE_BRACE_LEFT:
+		return parse_array(pos, last);
+	case Token::CURLY_BRACE_LEFT:
+		return parse_object(pos, last);
+	case Token::TRUE_:
+		return{ true, pos + 1 };
+	case Token::FALSE_:
+		return{ false, pos + 1 };
+	case Token::NULL_:
+		return{ nullptr, pos + 1 };
+	default:
+		throw std::runtime_error{ "expected a value" };
+	}
+}
+
+} // namespace
+
+
+void JsonValue::union_move(union_type &src, tag_type &src_tag, union_type &dst, tag_type &dst_tag)
+{
+	switch (src_tag) {
+	case NULL_:
+		new (&dst) nullptr_t{};
+		break;
+	case NUMBER:
+		new (&dst) number_type{ std::move(reinterpret_cast<number_type &>(src)) };
+		break;
+	case STRING:
+		new (&dst) string_type{ std::move(reinterpret_cast<string_type &>(src)) };
+		break;
+	case ARRAY:
+		new (&dst) array_type{ std::move(reinterpret_cast<array_type &>(src)) };
+		break;
+	case OBJECT:
+		new (&dst) object_type{ std::move(reinterpret_cast<object_type &>(src)) };
+		break;
+	case BOOL_:
+		new (&dst) boolean_type{ std::move(reinterpret_cast<boolean_type &>(src)) };
+		break;
+	}
+
+	dst_tag = src_tag;
+	src_tag = NULL_;
+}
+
+JsonValue::JsonValue(const JsonValue &other) :
+	m_tag{ NULL_ }
+{
+	switch (other.get_type()) {
+	case NULL_:
+		construct(nullptr);
+		break;
+	case NUMBER:
+		construct(other.number());
+		break;
+	case STRING:
+		construct(other.string());
+		break;
+	case ARRAY:
+		construct(other.array());
+		break;
+	case OBJECT:
+		construct(other.object());
+		break;
+	case BOOL_:
+		construct(other.boolean());
+		break;
+	}
+
+	m_tag = other.m_tag;
+}
+
+JsonValue::JsonValue(JsonValue &&other) :
+	m_tag{ NULL_ }
+{
+	swap(other);
+}
+
+JsonValue::~JsonValue()
+{
+	switch (get_type()) {
+	case NULL_:
+		destroy<nullptr_t>();
+		break;
+	case NUMBER:
+		destroy<number_type>();
+		break;
+	case STRING:
+		destroy<string_type>();
+		break;
+	case ARRAY:
+		destroy<array_type>();
+		break;
+	case OBJECT:
+		destroy<object_type>();
+		break;
+	case BOOL_:
+		destroy<boolean_type>();
+		break;
+	}
+}
+
+JsonValue &JsonValue::operator=(JsonValue other)
+{
+	swap(other);
+	return *this;
+}
+
+void JsonValue::swap(JsonValue &other)
+{
+	union_type tmp;
+	tag_type tmp_type;
+
+	union_move(other.m_data, other.m_tag, tmp, tmp_type);
+	union_move(m_data, m_tag, other.m_data, other.m_tag);
+	union_move(tmp, tmp_type, m_data, m_tag);
+}
+
+const JsonValue &JsonObject::operator[](const std::string &key) const
+{
+	static const JsonValue null_value{};
+
+	auto it = find(key);
+	return it == end() ? null_value : it->second;
+}
+
+
+namespace json {;
+
+JsonObject parse_document(const std::string &str)
+{
+	std::vector<Token> tokens = tokenize(str);
+
+	auto val = parse_object(tokens.begin(), tokens.end());
+
+	if (val.second != tokens.end())
+		throw std::runtime_error{ "unparsed document content" };
+
+	return std::move(val.first.object());
+}
+
+} // namespace json
---
+++ src/testcommon/json.h
@@ -0,0 +1,227 @@
+#pragma once
+
+#ifndef JSON_H_
+#define JSON_H_
+
+#include <cmath>
+#include <cstddef>
+#include <new>
+#include <map>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+class JsonValue;
+class JsonObject;
+
+class JsonObject : private std::map<std::string, JsonValue> {
+	typedef std::map<std::string, JsonValue> base_type;
+public:
+	using base_type::iterator;
+	using base_type::const_iterator;
+
+	using base_type::operator[];
+	using base_type::at;
+	using base_type::find;
+
+	using base_type::begin;
+	using base_type::end;
+	using base_type::cbegin;
+	using base_type::cend;
+
+	const JsonValue &operator[](const std::string &key) const;
+};
+
+class JsonValue {
+public:
+	typedef double number_type;
+	typedef std::string string_type;
+	typedef JsonObject object_type;
+	typedef std::vector<JsonValue> array_type;
+	typedef bool boolean_type;
+
+	enum tag_type {
+		NULL_,
+		NUMBER,
+		STRING,
+		OBJECT,
+		ARRAY,
+		BOOL_,
+	};
+private:
+#if defined(__GNUC__) && !(__GNUC__ > 5 || (__GNUC__ == 5 && __GNUC_MINOR__ >= 1))
+	typedef std::aligned_storage<64>::type union_type;
+#else
+	typedef std::aligned_union<0,
+		nullptr_t,
+		number_type,
+		string_type,
+		array_type,
+		object_type>::type union_type;
+#endif
+	union_type m_data;
+	tag_type m_tag;
+
+	static void union_move(union_type &src, tag_type &src_tag, union_type &dst, tag_type &dst_tag);
+
+	void check_type(tag_type type) const
+	{
+		if (m_tag != type)
+			throw std::invalid_argument{ "access as wrong type" };
+	}
+
+	template <class T>
+	void construct(T val)
+	{
+		new (&m_data) T{ std::move(val) };
+	}
+
+	template <class T>
+	void destroy()
+	{
+		reinterpret_cast<T *>(&m_data)->~T();
+	}
+
+	template <class T>
+	const T &as() const
+	{
+		return *reinterpret_cast<const T *>(&m_data);
+	}
+
+	template <class T>
+	T &as()
+	{
+		return *reinterpret_cast<T *>(&m_data);
+	}
+public:
+	JsonValue(nullptr_t = nullptr) : m_tag{ NULL_ }
+	{
+		construct(nullptr);
+	}
+
+	JsonValue(bool x) : m_tag{ BOOL_ }
+	{
+		construct(x);
+	}
+
+	JsonValue(number_type x) : m_tag{ NUMBER }
+	{
+		construct(x);
+	}
+
+	JsonValue(string_type str) : m_tag{ STRING }
+	{
+		construct(std::move(str));
+	}
+
+	JsonValue(array_type array) : m_tag{ ARRAY }
+	{
+		construct(std::move(array));
+	}
+
+	JsonValue(object_type object) : m_tag{ OBJECT }
+	{
+		construct(std::move(object));
+	}
+
+	JsonValue(const JsonValue &other);
+
+	JsonValue(JsonValue &&other);
+
+	~JsonValue();
+
+	JsonValue &operator=(JsonValue other);
+
+	explicit operator bool() const
+	{
+		return !is_null();
+	}
+
+	tag_type get_type() const
+	{
+		return m_tag;
+	}
+
+	bool is_null() const
+	{
+		return get_type() == NULL_;
+	}
+
+	const number_type &number() const
+	{
+		check_type(NUMBER);
+		return as<number_type>();
+	}
+
+	number_type &number()
+	{
+		check_type(NUMBER);
+		return as<number_type>();
+	}
+
+	long long integer() const
+	{
+		return llrint(number());
+	}
+
+	const string_type &string() const
+	{
+		check_type(STRING);
+		return as<string_type>();
+	}
+
+	string_type &string()
+	{
+		check_type(STRING);
+		return as<string_type>();
+	}
+
+	const object_type &object() const
+	{
+		check_type(OBJECT);
+		return as<object_type>();
+	}
+
+	object_type &object()
+	{
+		check_type(OBJECT);
+		return as<object_type>();
+	}
+
+	const array_type &array() const
+	{
+		check_type(ARRAY);
+		return as<array_type>();
+	}
+
+	array_type &array()
+	{
+		check_type(ARRAY);
+		return as<array_type>();
+	}
+
+	const boolean_type &boolean() const
+	{
+		check_type(BOOL_);
+		return as<boolean_type>();
+	}
+
+	boolean_type &boolean()
+	{
+		check_type(BOOL_);
+		return as<boolean_type>();
+	}
+
+	void swap(JsonValue &other);
+};
+
+
+namespace json {;
+
+JsonObject parse_document(const std::string &str);
+
+} // namespace json
+
+#endif // JSON_H_
