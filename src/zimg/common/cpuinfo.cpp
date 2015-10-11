3fbf2cc70a4205c9d1d6a8c00c3daf944b01f54a
sekrit-twc <noreply@example.com>
2015-10-11 10:02:12 -0700

    common: move query_x86_capabilities into a separate file

---
+++ src/zimg/common/cpuinfo.cpp
@@ -0,0 +1,62 @@
+#include "cpuinfo.h"
+
+#ifdef ZIMG_X86
+
+#if defined(_MSC_VER)
+  #include <intrin.h>
+#elif defined(__GNUC__)
+  #include <cpuid.h>
+#endif
+
+namespace zimg {;
+namespace {;
+
+/**
+ * Execute the CPUID instruction.
+ *
+ * @param regs array to receive eax, ebx, ecx, edx
+ * @param eax argument to instruction
+ * @param ecx argument to instruction
+ */
+inline void do_cpuid(int regs[4], int eax, int ecx)
+{
+#if defined(_MSC_VER)
+	__cpuidex(regs, eax, ecx);
+#elif defined(__GNUC__)
+	__cpuid_count(eax, ecx, regs[0], regs[1], regs[2], regs[3]);
+#else
+	regs[0] = 0;
+	regs[1] = 0;
+	regs[2] = 0;
+	regs[3] = 0;
+#endif
+}
+
+} // namespace
+
+
+X86Capabilities query_x86_capabilities()
+{
+	X86Capabilities caps = { 0 };
+	int regs[4] = { 0 };
+
+	do_cpuid(regs, 1, 0);
+	caps.sse   = !!(regs[3] & (1 << 25));
+	caps.sse2  = !!(regs[3] & (1 << 26));
+	caps.sse3  = !!(regs[2] & (1 << 0));
+	caps.ssse3 = !!(regs[2] & (1 << 9));
+	caps.fma   = !!(regs[2] & (1 << 12));
+	caps.sse41 = !!(regs[2] & (1 << 19));
+	caps.sse42 = !!(regs[2] & (1 << 20));
+	caps.avx   = !!(regs[2] & (1 << 28));
+	caps.f16c  = !!(regs[2] & (1 << 29));
+
+	do_cpuid(regs, 7, 0);
+	caps.avx2  = !!(regs[1] & (1 << 5));
+
+	return caps;
+}
+
+} // namespace zimg
+
+#endif // ZIMG_X86
--- src/zimg/common/cpuinfo.h
+++ src/zimg/common/cpuinfo.h
@@ -3,14 +3,6 @@
 #ifndef ZIMG_CPUINFO_H_
 #define ZIMG_CPUINFO_H_
 
-#ifdef ZIMG_X86
-  #if defined(_MSC_VER)
-    #include <intrin.h>
-  #elif defined(__GNUC__)
-    #include <cpuid.h>
-  #endif
-#endif // ZIMG_X86
-
 namespace zimg {;
 
 /**
@@ -45,52 +37,11 @@ struct X86Capabilities {
 };
 
 /**
- * Execute the CPUID instruction.
- *
- * @param regs array to receive eax, ebx, ecx, edx
- * @param eax argument to instruction
- * @param ecx argument to instruction
- */
-inline void do_cpuid(int regs[4], int eax, int ecx)
-{
-#if defined(_MSC_VER)
-	__cpuidex(regs, eax, ecx);
-#elif defined(__GNUC__)
-	__cpuid_count(eax, ecx, regs[0], regs[1], regs[2], regs[3]);
-#else
-	regs[0] = 0;
-	regs[1] = 0;
-	regs[2] = 0;
-	regs[3] = 0;
-#endif
-}
-
-/**
  * Get the x86 feature flags on the current CPU.
  *
  * @return capabilities
  */
-inline X86Capabilities query_x86_capabilities()
-{
-	X86Capabilities caps = { 0 };
-	int regs[4] = { 0 };
-
-	do_cpuid(regs, 1, 0);
-	caps.sse   = !!(regs[3] & (1 << 25));
-	caps.sse2  = !!(regs[3] & (1 << 26));
-	caps.sse3  = !!(regs[2] & (1 << 0));
-	caps.ssse3 = !!(regs[2] & (1 << 9));
-	caps.fma   = !!(regs[2] & (1 << 12));
-	caps.sse41 = !!(regs[2] & (1 << 19));
-	caps.sse42 = !!(regs[2] & (1 << 20));
-	caps.avx   = !!(regs[2] & (1 << 28));
-	caps.f16c  = !!(regs[2] & (1 << 29));
-
-	do_cpuid(regs, 7, 0);
-	caps.avx2 = !!(regs[1] & (1 << 5));
-
-	return caps;
-}
+X86Capabilities query_x86_capabilities();
 
 #endif // ZIMG_X86
 
