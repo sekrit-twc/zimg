0491d19d9e274c6c732a61ebe6df69a3a377b007
sekrit-twc <noreply@example.com>
2015-10-23 12:49:16 -0700

    unit: fix save/restore of FPU control word

--- test/extra/musl-libm/fpu_wrapper.c
+++ test/extra/musl-libm/fpu_wrapper.c
@@ -7,11 +7,13 @@ extern double _mycos(double y);
 extern double _mypow(double x, double y);
 extern float _mypowf(float x, float y);
 
-#if defined(_MSC_VER) && !defined(_M_X64)
-  #define fpu_set_single() _control87(_PC_24, MCW_PC)
+#if defined(_MSC_VER) && defined(_M_IX86)
+  #define fpu_save() _control87(0, 0)
+  #define fpu_set_single() _control87(_PC_24, _MCW_PC)
   #define fpu_set_double() _control87(_PC_53, _MCW_PC)
   #define fpu_restore(x) _control87((x), _MCW_PC);
 #else
+  #define fpu_save() 0
   #define fpu_set_single() 0
   #define fpu_set_double() 0
   #define fpu_restore(x)
@@ -19,32 +21,40 @@ extern float _mypowf(float x, float y);
 
 double mysin(double x)
 {
-	unsigned state = fpu_set_double();
-	double y = _mysin(x);
+	unsigned state = fpu_save();
+	double y;
+	fpu_set_double();
+	y = _mysin(x);
 	fpu_restore(state);
 	return y;
 }
 
 double mycos(double x)
 {
-	unsigned state = fpu_set_double();
-	double y = _mycos(x);
+	unsigned state = fpu_save();
+	double y;
+	fpu_set_double();
+	y = _mycos(x);
 	fpu_restore(state);
 	return y;
 }
 
 double mypow(double x, double y)
 {
-	unsigned state = fpu_set_double();
-	double z = _mypow(x, y);
+	unsigned state = fpu_save();
+	double z;
+	fpu_set_double();
+	z = _mypow(x, y);
 	fpu_restore(state);
 	return z;
 }
 
 float mypowf(float x, float y)
 {
-	unsigned state = fpu_set_single();
-	float z = _mypowf(x, y);
+	unsigned state = fpu_save();
+	float z;
+	fpu_set_single();
+	z = _mypowf(x, y);
 	fpu_restore(state);
 	return z;
 }
