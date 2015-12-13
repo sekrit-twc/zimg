06b8d60849031daf135446ad9c735c715329cac6
sekrit-twc <noreply@example.com>
2015-12-12 18:45:04 -0800

    Avoid function-level try/catch
    Causes an ICE in Visual Studio 2015 Update 1.

--- src/zimg/resize/resize.cpp
+++ src/zimg/resize/resize.cpp
@@ -40,67 +40,69 @@ ResizeConversion::ResizeConversion(unsigned src_width, unsigned src_height, Pixe
 {
 }
 
-auto ResizeConversion::create() const -> filter_pair try
+auto ResizeConversion::create() const -> filter_pair
 {
 	bool skip_h = (src_width == dst_width && shift_w == 0 && subwidth == src_width);
 	bool skip_v = (src_height == dst_height && shift_h == 0 && subheight == src_height);
 
-	if (skip_h && skip_v)
-		return{ ztd::make_unique<graph::CopyFilter>(src_width, src_height, type), nullptr };
+	try {
+		if (skip_h && skip_v)
+			return{ ztd::make_unique<graph::CopyFilter>(src_width, src_height, type), nullptr };
 
-	auto builder = ResizeImplBuilder{ src_width, src_height, type }.
-		set_depth(depth).
-		set_filter(filter).
-		set_cpu(cpu);
-	filter_pair ret{};
+		auto builder = ResizeImplBuilder{ src_width, src_height, type }.
+			set_depth(depth).
+			set_filter(filter).
+			set_cpu(cpu);
+		filter_pair ret{};
 
-	if (skip_h) {
-		ret.first = builder.set_horizontal(false).
-		                    set_dst_dim(dst_height).
-		                    set_shift(shift_h).
-		                    set_subwidth(subheight).
-		                    create();
-	} else if (skip_v) {
-		ret.first = builder.set_horizontal(true).
-		                    set_dst_dim(dst_width).
-		                    set_shift(shift_w).
-		                    set_subwidth(subwidth).
-		                    create();
-	} else {
-		bool h_first = resize_h_first((double)dst_width / src_width, (double)dst_height / src_height);
-
-		if (h_first) {
-			ret.first = builder.set_horizontal(true).
-			                    set_dst_dim(dst_width).
-			                    set_shift(shift_w).
-			                    set_subwidth(subwidth).
-			                    create();
-
-			builder.src_width = dst_width;
-			ret.second = builder.set_horizontal(false).
-			                     set_dst_dim(dst_height).
-			                     set_shift(shift_h).
-			                     set_subwidth(subheight).
-			                     create();
-		} else {
+		if (skip_h) {
 			ret.first = builder.set_horizontal(false).
-			                    set_dst_dim(dst_height).
-			                    set_shift(shift_h).
-			                    set_subwidth(subheight).
-			                    create();
+				set_dst_dim(dst_height).
+				set_shift(shift_h).
+				set_subwidth(subheight).
+				create();
+		} else if (skip_v) {
+			ret.first = builder.set_horizontal(true).
+				set_dst_dim(dst_width).
+				set_shift(shift_w).
+				set_subwidth(subwidth).
+				create();
+		} else {
+			bool h_first = resize_h_first((double)dst_width / src_width, (double)dst_height / src_height);
 
-			builder.src_height = dst_height;
-			ret.second = builder.set_horizontal(true).
-			                     set_dst_dim(dst_width).
-			                     set_shift(shift_w).
-			                     set_subwidth(subwidth).
-			                     create();
+			if (h_first) {
+				ret.first = builder.set_horizontal(true).
+					set_dst_dim(dst_width).
+					set_shift(shift_w).
+					set_subwidth(subwidth).
+					create();
+
+				builder.src_width = dst_width;
+				ret.second = builder.set_horizontal(false).
+					set_dst_dim(dst_height).
+					set_shift(shift_h).
+					set_subwidth(subheight).
+					create();
+			} else {
+				ret.first = builder.set_horizontal(false).
+					set_dst_dim(dst_height).
+					set_shift(shift_h).
+					set_subwidth(subheight).
+					create();
+
+				builder.src_height = dst_height;
+				ret.second = builder.set_horizontal(true).
+					set_dst_dim(dst_width).
+					set_shift(shift_w).
+					set_subwidth(subwidth).
+					create();
+			}
 		}
-	}
 
-	return ret;
-} catch (const std::bad_alloc &) {
-	throw error::OutOfMemory{};
+		return ret;
+	} catch (const std::bad_alloc &) {
+		throw error::OutOfMemory{};
+	}
 }
 
 } // namespace resize
