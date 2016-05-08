74318abd2a3be0a3ba67415f4da438a148f67f32
sekrit-twc <noreply@example.com>
2016-05-08 11:56:43 -0700

    testapp: add missing std namespace when calling isnan

--- src/testapp/table.cpp
+++ src/testapp/table.cpp
@@ -26,15 +26,15 @@ filter_uptr make_filter(double, double)
 
 filter_uptr make_bicubic_filter(double b, double c)
 {
-	b = isnan(b) ? 1.0 / 3.0 : b;
-	c = isnan(c) ? 1.0 / 3.0 : c;
+	b = std::isnan(b) ? 1.0 / 3.0 : b;
+	c = std::isnan(c) ? 1.0 / 3.0 : c;
 
 	return filter_uptr{ new zimg::resize::BicubicFilter{ b, c } };
 }
 
 filter_uptr make_lanczos_filter(double taps, double)
 {
-	taps = isnan(taps) ? 4.0 : taps;
+	taps = std::isnan(taps) ? 4.0 : taps;
 	return filter_uptr{ new zimg::resize::LanczosFilter{ static_cast<int>(std::floor(taps)) } };
 }
 
