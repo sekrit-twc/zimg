604e90c62b495e277fb094d6da92cf7f3c91298a
sekrit-twc <noreply@example.com>
2017-03-08 20:46:54 -0800

    resize: add AVX2 u16 horizontal resizer
    On Haswell, resizing 503x350 => 1005x350:
    
    Filter taps	cycles/pixel	cycles/tap	cycles/vector/tap
              2	        1.21	      0.61	             9.70
              4	        1.47	      0.37	             5.86
              6	        1.68	      0.28	             4.48
              8	        1.92	      0.24	             3.83

--- src/zimg/resize/resize_impl_avx2.cpp
+++ src/zimg/resize/resize_impl_avx2.cpp
@@ -156,6 +156,88 @@ inline FORCE_INLINE __m256i export_i30_u16(__m256i lo, __m256i hi, uint16_t limi
 	return lo;
 }
 
+inline FORCE_INLINE void mm256_transpose_16x16_epi16(__m256i &row0, __m256i &row1, __m256i &row2, __m256i &row3,
+                                                     __m256i &row4, __m256i &row5, __m256i &row6, __m256i &row7,
+                                                     __m256i &row8, __m256i &row9, __m256i &row10, __m256i &row11,
+                                                     __m256i &row12, __m256i &row13, __m256i &row14, __m256i &row15)
+{
+	__m256i t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15;
+	__m256i tt0, tt1, tt2, tt3, tt4, tt5, tt6, tt7, tt8, tt9, tt10, tt11, tt12, tt13, tt14, tt15;
+
+	t0 = _mm256_unpacklo_epi16(row0, row1);
+	t1 = _mm256_unpacklo_epi16(row2, row3);
+	t2 = _mm256_unpacklo_epi16(row4, row5);
+	t3 = _mm256_unpacklo_epi16(row6, row7);
+	t4 = _mm256_unpackhi_epi16(row0, row1);
+	t5 = _mm256_unpackhi_epi16(row2, row3);
+	t6 = _mm256_unpackhi_epi16(row4, row5);
+	t7 = _mm256_unpackhi_epi16(row6, row7);
+
+	tt0 = _mm256_unpacklo_epi32(t0, t1);
+	tt1 = _mm256_unpackhi_epi32(t0, t1);
+	tt2 = _mm256_unpacklo_epi32(t2, t3);
+	tt3 = _mm256_unpackhi_epi32(t2, t3);
+	tt4 = _mm256_unpacklo_epi32(t4, t5);
+	tt5 = _mm256_unpackhi_epi32(t4, t5);
+	tt6 = _mm256_unpacklo_epi32(t6, t7);
+	tt7 = _mm256_unpackhi_epi32(t6, t7);
+
+	t0 = _mm256_unpacklo_epi64(tt0, tt2);
+	t1 = _mm256_unpackhi_epi64(tt0, tt2);
+	t2 = _mm256_unpacklo_epi64(tt1, tt3);
+	t3 = _mm256_unpackhi_epi64(tt1, tt3);
+	t4 = _mm256_unpacklo_epi64(tt4, tt6);
+	t5 = _mm256_unpackhi_epi64(tt4, tt6);
+	t6 = _mm256_unpacklo_epi64(tt5, tt7);
+	t7 = _mm256_unpackhi_epi64(tt5, tt7);
+
+	t8 = _mm256_unpacklo_epi16(row8, row9);
+	t9 = _mm256_unpacklo_epi16(row10, row11);
+	t10 = _mm256_unpacklo_epi16(row12, row13);
+	t11 = _mm256_unpacklo_epi16(row14, row15);
+	t12 = _mm256_unpackhi_epi16(row8, row9);
+	t13 = _mm256_unpackhi_epi16(row10, row11);
+	t14 = _mm256_unpackhi_epi16(row12, row13);
+	t15 = _mm256_unpackhi_epi16(row14, row15);
+
+	tt8 = _mm256_unpacklo_epi32(t8, t9);
+	tt9 = _mm256_unpackhi_epi32(t8, t9);
+	tt10 = _mm256_unpacklo_epi32(t10, t11);
+	tt11 = _mm256_unpackhi_epi32(t10, t11);
+	tt12 = _mm256_unpacklo_epi32(t12, t13);
+	tt13 = _mm256_unpackhi_epi32(t12, t13);
+	tt14 = _mm256_unpacklo_epi32(t14, t15);
+	tt15 = _mm256_unpackhi_epi32(t14, t15);
+
+	t8 = _mm256_unpacklo_epi64(tt8, tt10);
+	t9 = _mm256_unpackhi_epi64(tt8, tt10);
+	t10 = _mm256_unpacklo_epi64(tt9, tt11);
+	t11 = _mm256_unpackhi_epi64(tt9, tt11);
+	t12 = _mm256_unpacklo_epi64(tt12, tt14);
+	t13 = _mm256_unpackhi_epi64(tt12, tt14);
+	t14 = _mm256_unpacklo_epi64(tt13, tt15);
+	t15 = _mm256_unpackhi_epi64(tt13, tt15);
+
+	row0 = _mm256_permute2f128_si256(t0, t8, 0x20);
+	row1 = _mm256_permute2f128_si256(t1, t9, 0x20);
+	row2 = _mm256_permute2f128_si256(t2, t10, 0x20);
+	row3 = _mm256_permute2f128_si256(t3, t11, 0x20);
+	row4 = _mm256_permute2f128_si256(t4, t12, 0x20);
+	row5 = _mm256_permute2f128_si256(t5, t13, 0x20);
+	row6 = _mm256_permute2f128_si256(t6, t14, 0x20);
+	row7 = _mm256_permute2f128_si256(t7, t15, 0x20);
+
+	row8 = _mm256_permute2f128_si256(t0, t8, 0x31);
+	row9 = _mm256_permute2f128_si256(t1, t9, 0x31);
+	row10 = _mm256_permute2f128_si256(t2, t10, 0x31);
+	row11 = _mm256_permute2f128_si256(t3, t11, 0x31);
+	row12 = _mm256_permute2f128_si256(t4, t12, 0x31);
+	row13 = _mm256_permute2f128_si256(t5, t13, 0x31);
+	row14 = _mm256_permute2f128_si256(t6, t14, 0x31);
+	row15 = _mm256_permute2f128_si256(t7, t15, 0x31);
+}
+
+
 template <class Traits, class T>
 void transpose_line_8x8(T *dst,
                         const T *src_p0, const T *src_p1, const T *src_p2, const T *src_p3,
@@ -190,6 +272,296 @@ void transpose_line_8x8(T *dst,
 		dst += 64;
 	}
 }
+
+void transpose_line_16x16_epi16(uint16_t *dst, const uint16_t * const *src, unsigned left, unsigned right)
+{
+	for (unsigned j = left; j < right; j += 16) {
+		__m256i x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15;
+
+		x0 = _mm256_load_si256((const __m256i *)(src[0] + j));
+		x1 = _mm256_load_si256((const __m256i *)(src[1] + j));
+		x2 = _mm256_load_si256((const __m256i *)(src[2] + j));
+		x3 = _mm256_load_si256((const __m256i *)(src[3] + j));
+		x4 = _mm256_load_si256((const __m256i *)(src[4] + j));
+		x5 = _mm256_load_si256((const __m256i *)(src[5] + j));
+		x6 = _mm256_load_si256((const __m256i *)(src[6] + j));
+		x7 = _mm256_load_si256((const __m256i *)(src[7] + j));
+		x8 = _mm256_load_si256((const __m256i *)(src[8] + j));
+		x9 = _mm256_load_si256((const __m256i *)(src[9] + j));
+		x10 = _mm256_load_si256((const __m256i *)(src[10] + j));
+		x11 = _mm256_load_si256((const __m256i *)(src[11] + j));
+		x12 = _mm256_load_si256((const __m256i *)(src[12] + j));
+		x13 = _mm256_load_si256((const __m256i *)(src[13] + j));
+		x14 = _mm256_load_si256((const __m256i *)(src[14] + j));
+		x15 = _mm256_load_si256((const __m256i *)(src[15] + j));
+
+		mm256_transpose_16x16_epi16(x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15);
+
+		_mm256_store_si256((__m256i *)(dst + 0), x0);
+		_mm256_store_si256((__m256i *)(dst + 16), x1);
+		_mm256_store_si256((__m256i *)(dst + 32), x2);
+		_mm256_store_si256((__m256i *)(dst + 48), x3);
+		_mm256_store_si256((__m256i *)(dst + 64), x4);
+		_mm256_store_si256((__m256i *)(dst + 80), x5);
+		_mm256_store_si256((__m256i *)(dst + 96), x6);
+		_mm256_store_si256((__m256i *)(dst + 112), x7);
+		_mm256_store_si256((__m256i *)(dst + 128), x8);
+		_mm256_store_si256((__m256i *)(dst + 144), x9);
+		_mm256_store_si256((__m256i *)(dst + 160), x10);
+		_mm256_store_si256((__m256i *)(dst + 176), x11);
+		_mm256_store_si256((__m256i *)(dst + 192), x12);
+		_mm256_store_si256((__m256i *)(dst + 208), x13);
+		_mm256_store_si256((__m256i *)(dst + 224), x14);
+		_mm256_store_si256((__m256i *)(dst + 240), x15);
+
+		dst += 256;
+	}
+}
+
+
+template <bool DoLoop, unsigned Tail>
+inline FORCE_INLINE __m256i resize_line8_h_u16_avx2_xiter(unsigned j,
+                                                          const unsigned *filter_left, const int16_t * RESTRICT filter_data, unsigned filter_stride, unsigned filter_width,
+                                                          const uint16_t * RESTRICT src_ptr, unsigned src_base, uint16_t limit)
+{
+	const __m256i i16_min = _mm256_set1_epi16(INT16_MIN);
+	const __m256i lim = _mm256_set1_epi16(limit + INT16_MIN);
+
+	const int16_t *filter_coeffs = filter_data + j * filter_stride;
+	const uint16_t *src_p = src_ptr + (filter_left[j] - src_base) * 16;
+
+	__m256i accum_lo = _mm256_setzero_si256();
+	__m256i accum_hi = _mm256_setzero_si256();
+	__m256i x0, x1, xl, xh, c, coeffs;
+
+	unsigned k_end = DoLoop ? floor_n(filter_width + 1, 8) : 0;
+
+	for (unsigned k = 0; k < k_end; k += 8) {
+		coeffs = _mm256_broadcastsi128_si256(_mm_load_si128((const __m128i *)(filter_coeffs + k)));
+
+		c = _mm256_shuffle_epi32(coeffs, _MM_SHUFFLE(0, 0, 0, 0));
+		x0 = _mm256_load_si256((const __m256i *)(src_p + (k + 0) * 16));
+		x1 = _mm256_load_si256((const __m256i *)(src_p + (k + 1) * 16));
+		x0 = _mm256_add_epi16(x0, i16_min);
+		x1 = _mm256_add_epi16(x1, i16_min);
+
+		xl = _mm256_unpacklo_epi16(x0, x1);
+		xh = _mm256_unpackhi_epi16(x0, x1);
+		xl = _mm256_madd_epi16(c, xl);
+		xh = _mm256_madd_epi16(c, xh);
+
+		accum_lo = _mm256_add_epi32(accum_lo, xl);
+		accum_hi = _mm256_add_epi32(accum_hi, xh);
+
+		c = _mm256_shuffle_epi32(coeffs, _MM_SHUFFLE(1, 1, 1, 1));
+		x0 = _mm256_load_si256((const __m256i *)(src_p + (k + 2) * 16));
+		x1 = _mm256_load_si256((const __m256i *)(src_p + (k + 3) * 16));
+		x0 = _mm256_add_epi16(x0, i16_min);
+		x1 = _mm256_add_epi16(x1, i16_min);
+
+		xl = _mm256_unpacklo_epi16(x0, x1);
+		xh = _mm256_unpackhi_epi16(x0, x1);
+		xl = _mm256_madd_epi16(c, xl);
+		xh = _mm256_madd_epi16(c, xh);
+
+		accum_lo = _mm256_add_epi32(accum_lo, xl);
+		accum_hi = _mm256_add_epi32(accum_hi, xh);
+
+		c = _mm256_shuffle_epi32(coeffs, _MM_SHUFFLE(2, 2, 2, 2));
+		x0 = _mm256_load_si256((const __m256i *)(src_p + (k + 4) * 16));
+		x1 = _mm256_load_si256((const __m256i *)(src_p + (k + 5) * 16));
+		x0 = _mm256_add_epi16(x0, i16_min);
+		x1 = _mm256_add_epi16(x1, i16_min);
+
+		xl = _mm256_unpacklo_epi16(x0, x1);
+		xh = _mm256_unpackhi_epi16(x0, x1);
+		xl = _mm256_madd_epi16(c, xl);
+		xh = _mm256_madd_epi16(c, xh);
+
+		accum_lo = _mm256_add_epi32(accum_lo, xl);
+		accum_hi = _mm256_add_epi32(accum_hi, xh);
+
+		c = _mm256_shuffle_epi32(coeffs, _MM_SHUFFLE(3, 3, 3, 3));
+		x0 = _mm256_load_si256((const __m256i *)(src_p + (k + 6) * 16));
+		x1 = _mm256_load_si256((const __m256i *)(src_p + (k + 7) * 16));
+		x0 = _mm256_add_epi16(x0, i16_min);
+		x1 = _mm256_add_epi16(x1, i16_min);
+
+		xl = _mm256_unpacklo_epi16(x0, x1);
+		xh = _mm256_unpackhi_epi16(x0, x1);
+		xl = _mm256_madd_epi16(c, xl);
+		xh = _mm256_madd_epi16(c, xh);
+
+		accum_lo = _mm256_add_epi32(accum_lo, xl);
+		accum_hi = _mm256_add_epi32(accum_hi, xh);
+	}
+
+	if (Tail >= 2) {
+		coeffs = _mm256_broadcastsi128_si256(_mm_load_si128((const __m128i *)(filter_coeffs + k_end)));
+
+		c = _mm256_shuffle_epi32(coeffs, _MM_SHUFFLE(0, 0, 0, 0));
+		x0 = _mm256_load_si256((const __m256i *)(src_p + (k_end + 0) * 16));
+		x1 = _mm256_load_si256((const __m256i *)(src_p + (k_end + 1) * 16));
+		x0 = _mm256_add_epi16(x0, i16_min);
+		x1 = _mm256_add_epi16(x1, i16_min);
+
+		xl = _mm256_unpacklo_epi16(x0, x1);
+		xh = _mm256_unpackhi_epi16(x0, x1);
+		xl = _mm256_madd_epi16(c, xl);
+		xh = _mm256_madd_epi16(c, xh);
+
+		accum_lo = _mm256_add_epi32(accum_lo, xl);
+		accum_hi = _mm256_add_epi32(accum_hi, xh);
+	}
+
+	if (Tail >= 4) {
+		c = _mm256_shuffle_epi32(coeffs, _MM_SHUFFLE(1, 1, 1, 1));
+		x0 = _mm256_load_si256((const __m256i *)(src_p + (k_end + 2) * 16));
+		x1 = _mm256_load_si256((const __m256i *)(src_p + (k_end + 3) * 16));
+		x0 = _mm256_add_epi16(x0, i16_min);
+		x1 = _mm256_add_epi16(x1, i16_min);
+
+		xl = _mm256_unpacklo_epi16(x0, x1);
+		xh = _mm256_unpackhi_epi16(x0, x1);
+		xl = _mm256_madd_epi16(c, xl);
+		xh = _mm256_madd_epi16(c, xh);
+
+		accum_lo = _mm256_add_epi32(accum_lo, xl);
+		accum_hi = _mm256_add_epi32(accum_hi, xh);
+	}
+
+	if (Tail >= 6) {
+		c = _mm256_shuffle_epi32(coeffs, _MM_SHUFFLE(2, 2, 2, 2));
+		x0 = _mm256_load_si256((const __m256i *)(src_p + (k_end + 4) * 16));
+		x1 = _mm256_load_si256((const __m256i *)(src_p + (k_end + 5) * 16));
+		x0 = _mm256_add_epi16(x0, i16_min);
+		x1 = _mm256_add_epi16(x1, i16_min);
+
+		xl = _mm256_unpacklo_epi16(x0, x1);
+		xh = _mm256_unpackhi_epi16(x0, x1);
+		xl = _mm256_madd_epi16(c, xl);
+		xh = _mm256_madd_epi16(c, xh);
+
+		accum_lo = _mm256_add_epi32(accum_lo, xl);
+		accum_hi = _mm256_add_epi32(accum_hi, xh);
+	}
+
+	if (Tail >= 8) {
+		c = _mm256_shuffle_epi32(coeffs, _MM_SHUFFLE(3, 3, 3, 3));
+		x0 = _mm256_load_si256((const __m256i *)(src_p + (k_end + 6) * 16));
+		x1 = _mm256_load_si256((const __m256i *)(src_p + (k_end + 7) * 16));
+		x0 = _mm256_add_epi16(x0, i16_min);
+		x1 = _mm256_add_epi16(x1, i16_min);
+
+		xl = _mm256_unpacklo_epi16(x0, x1);
+		xh = _mm256_unpackhi_epi16(x0, x1);
+		xl = _mm256_madd_epi16(c, xl);
+		xh = _mm256_madd_epi16(c, xh);
+
+		accum_lo = _mm256_add_epi32(accum_lo, xl);
+		accum_hi = _mm256_add_epi32(accum_hi, xh);
+	}
+
+	accum_lo = export_i30_u16(accum_lo, accum_hi, limit);
+	accum_lo = _mm256_min_epi16(accum_lo, lim);
+	accum_lo = _mm256_sub_epi16(accum_lo, i16_min);
+	return accum_lo;
+}
+
+template <bool DoLoop, unsigned Tail>
+void resize_line8_h_u16_avx2(const unsigned *filter_left, const int16_t * RESTRICT filter_data, unsigned filter_stride, unsigned filter_width,
+                             const uint16_t * RESTRICT src_ptr, uint16_t * const *dst_ptr, unsigned src_base, unsigned left, unsigned right, uint16_t limit)
+{
+	unsigned vec_left = ceil_n(left, 16);
+	unsigned vec_right = floor_n(right, 16);
+
+#define XITER resize_line8_h_u16_avx2_xiter<DoLoop, Tail>
+#define XARGS filter_left, filter_data, filter_stride, filter_width, src_ptr, src_base, limit
+	for (unsigned j = left; j < vec_left; ++j) {
+		__m256i x = XITER(j, XARGS);
+
+		mm_scatter_epi16(dst_ptr[0] + j, dst_ptr[1] + j, dst_ptr[2] + j, dst_ptr[3] + j, dst_ptr[4] + j, dst_ptr[5] + j, dst_ptr[6] + j, dst_ptr[7] + j, _mm256_castsi256_si128(x));
+		mm_scatter_epi16(dst_ptr[8] + j, dst_ptr[9] + j, dst_ptr[10] + j, dst_ptr[11] + j, dst_ptr[12] + j, dst_ptr[13] + j, dst_ptr[14] + j, dst_ptr[15] + j, _mm256_extractf128_si256(x, 1));
+	}
+
+	for (unsigned j = vec_left; j < vec_right; j += 16) {
+		uint16_t cache alignas(32)[16][16];
+		__m256i x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15;
+
+		for (unsigned jj = j; jj < j + 16; ++jj) {
+			__m256i x = XITER(jj, XARGS);
+			_mm256_store_si256((__m256i *)cache[jj - j], x);
+		}
+
+		x0 = _mm256_load_si256((const __m256i *)cache[0]);
+		x1 = _mm256_load_si256((const __m256i *)cache[1]);
+		x2 = _mm256_load_si256((const __m256i *)cache[2]);
+		x3 = _mm256_load_si256((const __m256i *)cache[3]);
+		x4 = _mm256_load_si256((const __m256i *)cache[4]);
+		x5 = _mm256_load_si256((const __m256i *)cache[5]);
+		x6 = _mm256_load_si256((const __m256i *)cache[6]);
+		x7 = _mm256_load_si256((const __m256i *)cache[7]);
+		x8 = _mm256_load_si256((const __m256i *)cache[8]);
+		x9 = _mm256_load_si256((const __m256i *)cache[9]);
+		x10 = _mm256_load_si256((const __m256i *)cache[10]);
+		x11 = _mm256_load_si256((const __m256i *)cache[11]);
+		x12 = _mm256_load_si256((const __m256i *)cache[12]);
+		x13 = _mm256_load_si256((const __m256i *)cache[13]);
+		x14 = _mm256_load_si256((const __m256i *)cache[14]);
+		x15 = _mm256_load_si256((const __m256i *)cache[15]);
+
+		mm256_transpose_16x16_epi16(x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15);
+
+		_mm256_store_si256((__m256i *)(dst_ptr[0] + j), x0);
+		_mm256_store_si256((__m256i *)(dst_ptr[1] + j), x1);
+		_mm256_store_si256((__m256i *)(dst_ptr[2] + j), x2);
+		_mm256_store_si256((__m256i *)(dst_ptr[3] + j), x3);
+		_mm256_store_si256((__m256i *)(dst_ptr[4] + j), x4);
+		_mm256_store_si256((__m256i *)(dst_ptr[5] + j), x5);
+		_mm256_store_si256((__m256i *)(dst_ptr[6] + j), x6);
+		_mm256_store_si256((__m256i *)(dst_ptr[7] + j), x7);
+		_mm256_store_si256((__m256i *)(dst_ptr[8] + j), x8);
+		_mm256_store_si256((__m256i *)(dst_ptr[9] + j), x9);
+		_mm256_store_si256((__m256i *)(dst_ptr[10] + j), x10);
+		_mm256_store_si256((__m256i *)(dst_ptr[11] + j), x11);
+		_mm256_store_si256((__m256i *)(dst_ptr[12] + j), x12);
+		_mm256_store_si256((__m256i *)(dst_ptr[13] + j), x13);
+		_mm256_store_si256((__m256i *)(dst_ptr[14] + j), x14);
+		_mm256_store_si256((__m256i *)(dst_ptr[15] + j), x15);
+	}
+
+	for (unsigned j = vec_right; j < right; ++j) {
+		__m256i x = XITER(j, XARGS);
+
+		mm_scatter_epi16(dst_ptr[0] + j, dst_ptr[1] + j, dst_ptr[2] + j, dst_ptr[3] + j, dst_ptr[4] + j, dst_ptr[5] + j, dst_ptr[6] + j, dst_ptr[7] + j, _mm256_castsi256_si128(x));
+		mm_scatter_epi16(dst_ptr[8] + j, dst_ptr[9] + j, dst_ptr[10] + j, dst_ptr[11] + j, dst_ptr[12] + j, dst_ptr[13] + j, dst_ptr[14] + j, dst_ptr[15] + j, _mm256_extractf128_si256(x, 1));
+	}
+
+#undef XITER
+#undef XARGS
+}
+
+const decltype(&resize_line8_h_u16_avx2<false, 0>) resize_line8_h_u16_avx2_jt_small[] = {
+	resize_line8_h_u16_avx2<false, 2>,
+	resize_line8_h_u16_avx2<false, 2>,
+	resize_line8_h_u16_avx2<false, 4>,
+	resize_line8_h_u16_avx2<false, 4>,
+	resize_line8_h_u16_avx2<false, 6>,
+	resize_line8_h_u16_avx2<false, 6>,
+	resize_line8_h_u16_avx2<false, 8>,
+	resize_line8_h_u16_avx2<false, 8>,
+};
+
+const decltype(&resize_line8_h_u16_avx2<false, 0>) resize_line8_h_u16_avx2_jt_large[] = {
+	resize_line8_h_u16_avx2<true, 0>,
+	resize_line8_h_u16_avx2<true, 2>,
+	resize_line8_h_u16_avx2<true, 2>,
+	resize_line8_h_u16_avx2<true, 4>,
+	resize_line8_h_u16_avx2<true, 4>,
+	resize_line8_h_u16_avx2<true, 6>,
+	resize_line8_h_u16_avx2<true, 6>,
+	resize_line8_h_u16_avx2<true, 0>,
+};
 
 template <class Traits, unsigned FWidth, unsigned Tail>
 inline FORCE_INLINE __m256 resize_line8_h_fp_avx2_xiter(unsigned j,
@@ -638,6 +1010,61 @@ const typename resize_line_v_fp_avx2_jt<Traits>::func_type resize_line_v_fp_avx2
 };
 
 
+class ResizeImplH_U16_AVX2 final : public ResizeImplH {
+	decltype(&resize_line8_h_u16_avx2<false, 0>) m_func;
+	uint16_t m_pixel_max;
+public:
+	ResizeImplH_U16_AVX2(const FilterContext &filter, unsigned height, unsigned depth) :
+		ResizeImplH(filter, image_attributes{ filter.filter_rows, height, PixelType::WORD }),
+		m_func{},
+		m_pixel_max{ static_cast<uint16_t>((1UL << depth) - 1) }
+	{
+		if (filter.filter_width > 8)
+			m_func = resize_line8_h_u16_avx2_jt_large[filter.filter_width % 8];
+		else
+			m_func = resize_line8_h_u16_avx2_jt_small[filter.filter_width - 1];
+	}
+
+	unsigned get_simultaneous_lines() const override { return 16; }
+
+	size_t get_tmp_size(unsigned left, unsigned right) const override
+	{
+		auto range = get_required_col_range(left, right);
+
+		try {
+			checked_size_t size = (static_cast<checked_size_t>(range.second) - floor_n(range.first, 16) + 16) * sizeof(uint16_t) * 16;
+			return size.get();
+		} catch (const std::overflow_error &) {
+			error::throw_<error::OutOfMemory>();
+		}
+	}
+
+	void process(void *, const graph::ImageBuffer<const void> *src, const graph::ImageBuffer<void> *dst, void *tmp, unsigned i, unsigned left, unsigned right) const override
+	{
+		auto src_buf = graph::static_buffer_cast<const uint16_t>(*src);
+		auto dst_buf = graph::static_buffer_cast<uint16_t>(*dst);
+		auto range = get_required_col_range(left, right);
+
+		const uint16_t *src_ptr[16] = { 0 };
+		uint16_t *dst_ptr[16] = { 0 };
+		uint16_t *transpose_buf = static_cast<uint16_t *>(tmp);
+		unsigned height = get_image_attributes().height;
+
+		for (unsigned n = 0; n < 16; ++n) {
+			src_ptr[n] = src_buf[std::min(i + n, height - 1)];
+		}
+
+		transpose_line_16x16_epi16(transpose_buf, src_ptr, floor_n(range.first, 16), ceil_n(range.second, 16));
+
+		for (unsigned n = 0; n < 16; ++n) {
+			dst_ptr[n] = dst_buf[std::min(i + n, height - 1)];
+		}
+
+		m_func(m_filter.left.data(), m_filter.data_i16.data(), m_filter.stride_i16, m_filter.filter_width,
+		       transpose_buf, dst_ptr, floor_n(range.first, 16), left, right, m_pixel_max);
+	}
+};
+
 template <class Traits>
 class ResizeImplH_FP_AVX2 final : public ResizeImplH {
 	typedef typename Traits::pixel_type pixel_type;
@@ -814,7 +1241,9 @@ std::unique_ptr<graph::ImageFilter> create_resize_impl_h_avx2(const FilterContex
 {
 	std::unique_ptr<graph::ImageFilter> ret;
 
-	if (type == PixelType::HALF)
+	if (type == PixelType::WORD)
+		ret = ztd::make_unique<ResizeImplH_U16_AVX2>(context, height, depth);
+	else if (type == PixelType::HALF)
 		ret = ztd::make_unique<ResizeImplH_FP_AVX2<f16_traits>>(context, height);
 	else if (type == PixelType::FLOAT)
 		ret = ztd::make_unique<ResizeImplH_FP_AVX2<f32_traits>>(context, height);
