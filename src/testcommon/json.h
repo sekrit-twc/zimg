08ff0100b33f8c74cf750635482705daca43e0e7
sekrit-twc <noreply@example.com>
2023-05-09 22:58:47 -0700

    testcommon: store JSON values in std::variant

--- src/testcommon/json.h
+++ src/testcommon/json.h
@@ -9,6 +9,7 @@
 #include <string>
 #include <type_traits>
 #include <utility>
+#include <variant>
 #include <vector>
 
 namespace json {
@@ -62,8 +63,6 @@ typedef std::vector<Value> Array;
 
 class Value {
 public:
-	struct static_initializer_tag {};
-
 	enum tag_type {
 		UNDEFINED,
 		NULL_,
@@ -74,71 +73,44 @@ public:
 		BOOL_,
 	};
 private:
-	typedef std::aligned_union_t<0,
+	std::variant<
+		std::monostate,
 		std::nullptr_t,
 		double,
 		std::string,
+		Object,
 		Array,
-		Object
-	> union_type;
-
-	tag_type m_tag;
-	union_type m_union;
-
-	static void move_helper(tag_type &src_tag, union_type &src_union,
-	                        tag_type &dst_tag, union_type &dst_union) noexcept;
-
-	template <class T>
-	void construct(T x) { new (&m_union) T{ std::move(x) }; }
-
-	template <class T>
-	void destroy() noexcept { reinterpret_cast<T &>(m_union).~T(); }
-
-	template <class T>
-	T &as() noexcept { return reinterpret_cast<T &>(m_union); }
-
-	template <class T>
-	const T &as() const noexcept { return reinterpret_cast<const T &>(m_union); }
+		bool
+	> m_variant;
 
 	void check_tag(tag_type tag) const
 	{
-		if (get_type() != tag)
+		if (m_variant.index() != tag)
 			throw std::invalid_argument{ "access as wrong type" };
 	}
-
 public:
-	constexpr explicit Value(static_initializer_tag) noexcept : m_tag{ UNDEFINED }, m_union{} {}
+	constexpr Value() noexcept {}
 
-	Value() noexcept : m_tag{ UNDEFINED } {}
-
-	explicit Value(std::nullptr_t) noexcept : m_tag{ NULL_ } {}
-	explicit Value(double val) noexcept : m_tag{ NUMBER } { construct(val); }
-	explicit Value(std::string val) : m_tag{ STRING } { construct(std::move(val)); }
-	explicit Value(Array val) : m_tag{ ARRAY } { construct(std::move(val)); }
-	explicit Value(Object val) : m_tag{ OBJECT } { construct(std::move(val)); }
-	explicit Value(bool val) noexcept : m_tag{ BOOL_ } { construct(val); }
+	explicit Value(std::nullptr_t) noexcept : m_variant{ nullptr } {}
+	explicit Value(double val) noexcept : m_variant{ val } {}
+	explicit Value(std::string val) : m_variant{ std::move(val) } {}
+	explicit Value(Array val) : m_variant{ std::move(val) } {}
+	explicit Value(Object val) : m_variant{ std::move(val) } {}
+	explicit Value(bool val) noexcept : m_variant{ val } {}
 
 	template <class T, class = std::enable_if_t<std::is_integral_v<T>>>
 	explicit Value(T val) noexcept : Value{ static_cast<double>(val) } {}
-
-	Value(const Value &other);
-	Value(Value &&other) noexcept;
-
-	~Value();
-
-	Value &operator=(const Value &other);
-	Value &operator=(Value &&other) noexcept;
 
 	explicit operator bool() const noexcept { return !is_undefined() && !is_null(); }
 
 	bool is_undefined() const noexcept { return get_type() == UNDEFINED; }
 	bool is_null() const noexcept { return get_type() == NULL_; }
 
-	tag_type get_type() const noexcept { return m_tag; }
+	tag_type get_type() const noexcept { return static_cast<tag_type>(m_variant.index()); }
 
 #define JSON_VALUE_GET_SET(T, name, tag) \
-  const T &name() const { check_tag(tag); return as<T>(); } \
-  T &name() { check_tag(tag); return as<T>(); }
+  const T &name() const { check_tag(tag); return std::get<T>(m_variant); } \
+  T &name() { check_tag(tag); return std::get<T>(m_variant); }
 
 	JSON_VALUE_GET_SET(double, number, NUMBER)
 	JSON_VALUE_GET_SET(std::string, string, STRING)
@@ -150,12 +122,12 @@ public:
 
 	long long integer() const { return std::llrint(number()); }
 
-	friend void swap(Value &a, Value &b) noexcept;
+	friend void swap(Value &a, Value &b) noexcept { std::swap(a.m_variant, b.m_variant); }
 };
 
 inline const Value &Object::operator[](const std::string &key) const noexcept
 {
-	static const Value null_value{ Value::static_initializer_tag{} };
+	static const Value null_value;
 
 	auto it = find(key);
 	return it == end() ? null_value : it->second;
