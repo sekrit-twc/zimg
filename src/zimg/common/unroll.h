a0fac0fe4d2efc9e4b25c0f0e11ef41d7d57e2d2
sekrit-twc <noreply@example.com>
2023-11-08 21:54:23 -0800

    common: use std::integral_constant

--- src/zimg/common/unroll.h
+++ src/zimg/common/unroll.h
@@ -4,6 +4,7 @@
 #define ZIMG_UNROLL_H_
 
 #include <cstddef>
+#include <type_traits>
 #include <utility>
 #include "ccdep.h"
 
@@ -11,16 +12,10 @@ namespace zimg {
 
 namespace detail {
 
-template <size_t I> struct Index {
-	static constexpr size_t i = I;
-
-	constexpr operator size_t() const { return i; }
-};
-
 template <class T, size_t ...Idx>
 inline FORCE_INLINE void do_unroll(T f, std::index_sequence<Idx...>)
 {
-	(f(Index<Idx>()), ...);
+	(f(std::integral_constant<size_t, Idx>{}), ...);
 }
 
 } // namespace detail
